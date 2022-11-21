391e0a73838413706d197c5e64f8393075ba294d
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2022-11-21 16:36:37 +0530

    implemnting backtotop component

---
+++ src/backtotop/BackToTop.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./BackToTop.css";
+import { FaChevronUp } from "react-icons/fa";
+
+const BackToTop = () => {
+  const [backToTop, setBackToTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTop(true);
+      } else {
+        setBackToTop(false);
+      }
+    });
+  }, []);
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      {backToTop && (
+        <button className="btn-style" onClick={scrollUp}>
+          <FaChevronUp />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default BackToTop;
